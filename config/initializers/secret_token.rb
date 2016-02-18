8225007077539903a785896e8a05e0dca59ddf7e
NamSteven <StevenNam@NamStevendeMacBook-Pro.local>
2016-02-18 21:18:22 +0800

    Create Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+MedicalboxApi::Application.config.secret_token = 'f2f5a78da05349ecf2cfa8f34c0602514899073de7957a11f09099bc175136db0c63de503df962dcd085b0bfd77873b08b576390ea9e553fb188970aa69752bf'
